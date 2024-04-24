bdfc511858f04479caffdc272cd6159a1e679b91
Matheus Penna <matheus.penna@blitzpay.com.br>
2024-04-24 19:29:19 -0300

    create tests to render page, fill fields correctly and send to API

--- src/frontend/cypress.config.js
+++ src/frontend/cypress.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  chromeWebSecurity: false,
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:5173'
   }
 })
--- src/frontend/cypress/e2e/page.cy.js
+++ src/frontend/cypress/e2e/page.cy.js
@@ -1,8 +1,32 @@
 // https://on.cypress.io/api
 
-describe('First page load', () => {
+describe('Page tests', () => {
   it('visits the app root url', () => {
     cy.visit('/')
     cy.contains('h1', 'Seja bem vindo(a)')
+  })
+
+  it('fill fields correctly and click to register', () => {
+    // Step 1
+    cy.visit('/')
+    cy.get('#email').type('test@test.com')
+    cy.get('[type="radio"]').check('pf')
+    cy.get('button[type="submit"]').click()
+    
+    // Step 2
+    cy.get('#name').type('Dev frontend test')
+    cy.get('#cpf').type('06914804001')
+    cy.get('#birth_date').type('14041995')
+    cy.get('#phone').type('75991025971')
+    cy.get('button[type="submit"]').click()
+    
+    // Step 3
+    cy.get('#password').type('102030@Pass')
+    cy.get('button[type="submit"]').click()
+
+    // Step 4
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('h1', 'Registro concluído')
   })
 })
