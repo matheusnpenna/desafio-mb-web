211ba17919c228ddaa48b9841768546726f383cc
Matheus Penna <matheus.penna@blitzpay.com.br>
2024-04-24 23:41:21 -0300

    try to dinamic set baseURL

--- src/frontend/src/composables/api.js
+++ src/frontend/src/composables/api.js
@@ -3,14 +3,17 @@ const DEFAULT_OPTIONS = {
     "Content-Type": "application/json",
   }
 }
-const config = {
-  baseURL: 'http://localhost:8989/api'
+let baseURL = 'https://matheuspenna-desafio-mb-web.vercel.app/api'
+
+if (document.location.href.indexOf("localhost") > -1 || process.env.NODE_ENV === "development") {
+  baseURL = 'http://localhost:8989/api';
 }
+
 export function useAPI() {
 
   const postRegister = (data) => {
     return new Promise((resolve, reject) => {
-      return fetch(`${config.baseURL}/register`, {
+      return fetch(`${baseURL}/register`, {
         ...DEFAULT_OPTIONS,
         method: 'POST',
         body: JSON.stringify(data)
