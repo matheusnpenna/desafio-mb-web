173e2657b7fcf8b9b7fed7e57d4e9bbb16e4b9f7
Matheus Penna <matheus.penna@blitzpay.com.br>
2024-04-25 01:29:03 -0300

    test both as PF and PJ in e2e tests

--- src/frontend/cypress/e2e/page.cy.js
+++ src/frontend/cypress/e2e/page.cy.js
@@ -6,7 +6,7 @@ describe('Page tests', () => {
     cy.contains('h1', 'Seja bem vindo(a)')
   })
 
-  it('fill fields correctly and click to register', () => {
+  it('fill fields correctly and click to register as PF', () => {
     // Step 1
     cy.visit('/')
     cy.get('#email').type('test@test.com')
@@ -16,6 +16,30 @@ describe('Page tests', () => {
     // Step 2
     cy.get('#name').type('Dev frontend test')
     cy.get('#document').type('03785850530')
+    cy.get('#birth_date').type('14041995')
+    cy.get('#phone').type('75991025971')
+    cy.get('button[type="submit"]').click()
+    
+    // Step 3
+    cy.get('#password').type('102030@Pass')
+    cy.get('button[type="submit"]').click()
+
+    // Step 4
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('h1', 'Registro concluído')
+  })
+
+  it('fill fields correctly and click to register as PJ', () => {
+    // Step 1
+    cy.visit('/')
+    cy.get('#email').type('test@test.com')
+    cy.get('[data-test-id="radio-input-pj"]').click()
+    cy.get('button[type="submit"]').click()
+    
+    // Step 2
+    cy.get('#name').type('Dev frontend test')
+    cy.get('#document').type('18213434000135')
     cy.get('#birth_date').type('14041995')
     cy.get('#phone').type('75991025971')
     cy.get('button[type="submit"]').click()
